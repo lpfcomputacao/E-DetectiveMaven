6dc5ffe723a3cc231bf8f10decd5016c63924ccf
Otávio Neves Lara <otavioneveslara@gmail.com>
2018-01-28 12:59:35 -0200

    detective created

--- src/main/java/models/Detective.java
+++ src/main/java/models/Detective.java
@@ -1,11 +1,39 @@
 package models;
 
-public class Detective implements Runnable {
+import java.util.ArrayList;
 
+import DTO.Point;
+import DTO.User;
+
+public class Detective implements Runnable {
+	private User user;
+	private ArrayList<String> notificacoes;
+	public Detective(User user) {
+		this.user = user;
+	}
 	@Override
 	public void run() {
-		
-		
+		while(true){
+			verificarDistanciaDoMarcador();
+			updateUsuario();
+		}
+	}
+	private void updateUsuario() {
+		//procura a informações desse usuário (ou puxa tudo de novo ou puxa só o marcador e posição)
+	}
+	private void verificarDistanciaDoMarcador() {
+		Point currentPosition = user.getCurrentPosition();
+		Point marker = user.getMarkerPosition();
+		double distancia = distanciaEuclidiana(currentPosition, marker);
+		if( distancia < 50.0) {
+			notificacoes.add("Seu rastreado está a "+distancia+" do maracador ("+marker.getX()+", "+marker.getY()+")");
+		}
+	}
+	public ArrayList<String> getNotificacoes() {
+		return notificacoes;
+	}
+	private double distanciaEuclidiana(Point p1, Point p2) {
+		return Math.sqrt(Math.pow((p1.getX() - p2.getX()), 2) + Math.pow((p1.getY() - p2.getY()),2));
 	}
 
 }
--- src/main/java/models/DetectivesManager.java
+++ src/main/java/models/DetectivesManager.java
@@ -1,24 +1,55 @@
 package models;
 
+import java.net.ServerSocket;
 import java.util.HashMap;
+
+import DTO.User;
 
 public class DetectivesManager implements Runnable {
 
-	public static HashMap<String, Runnable> detectivesHashMap;
-	
-	
-	public DetectivesManager () {}
-	
+	public static HashMap<User, Detective> detectives;
+	ServerSocket serverSocket;
+	public DetectivesManager() {
+	}
+
 	@Override
 	public void run() {
-		
-		//detectivesHashMap
-		// iniciar número de threads igual ao número de usuários 
-		
-		// Todas as threads vão ficar responsáveis por um usuário 
-		
-		// haverá um listener nesta classe responsável em ser o intermediário entre a requisição do usuário e o conteúdo das threads 
-		
+
+		// detectivesHashMap
+		// iniciar número de threads igual ao número de usuários
+
+		// Todas as threads vão ficar responsáveis por um usuário
+
+		// haverá um listener nesta classe responsável em ser o intermediário entre a
+		// requisição do usuário e o conteúdo das threads
+		observingNewUser();
+	}
+	private Detective getDetectiveByUser(User user) {
+		return detectives.get(user);
+	}
+	private void observingNewUser() {
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				while (true) {
+					User user = null;
+					try {
+						Thread.sleep(30000);
+						// Aqui o Lucas fica procurando os novos usuários no banco de dados7
+						//if(achou usuário){
+							creatDetective(user);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		});
+	}
+
+	private void creatDetective(User user) {
+		Detective detective = new Detective(user);
+		detectives.put(user, detective);
+		new Thread(detective).start();
 	}
 
 }
